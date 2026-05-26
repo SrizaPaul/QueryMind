21417f32d30447f3a70c9acee0847c38c7014eba
Sriza Paul <srizapaulkolkatabkp@gmail.com>
2026-05-26 19:56:49 +0530

    fixed render port issue

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,13 @@ const authRoutes =
 require('./src/routes/auth.routes');
 app.use('/api/auth', authRoutes);
 
-const PORT = 3000;
+const PORT =
+  process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+  console.log(
+    `Server running on port ${PORT}`
+  );
+
 });
